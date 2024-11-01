b1668d52a4ef4341ef67fea4704f0ebc0783c4f5
AVKydin <tlalok@ukr.net>
2024-11-01 10:00:20 +0200

    logOut

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,16 +9,18 @@ import OutsideClickHandler from 'react-outside-click-handler';
 import { UserStatus } from '../../common/user-status.enum';
 import 'react-toastify/dist/ReactToastify.css';
 import './Header.css';
+import {logOut} from "../../services/auth.services";
+import {useNavigate} from "react-router-dom";
 
 const Header = (): JSX.Element => {
-    const { appState,
+    const { appState, setState
     } = useContext(AppContext);
     const user = appState.user;
     const userUsername = appState.userData?.username;
 
     const [isStatusOpen, setIsStatusOpen] = useState(false);
     const [userData, setUserData] = useState<User>({} as User);
-
+    const navigate = useNavigate();
     const [activeButton, setActiveButton] = useState<HTMLElement>();
     const [width, setWidth] = useState(window.innerWidth);
     const [isNavMenu, setIsNavMenu] = useState(true);
@@ -62,11 +64,22 @@ const Header = (): JSX.Element => {
         updateUserStatus(userUsername!, UserStatus.ONLINE).catch(console.error);
     };
 
+    const handleLogOut = () => {
+        setState({
+            user: null,
+            userData: null,
+        });
+
+        logOut().catch(console.error);
+        updateUserStatus(userUsername!, UserStatus.OFFLINE).catch(console.error);
+        navigate('/');
+    };
+
     return (
         <>
             <header id='header'>
 
-
+                <button onClick={handleLogOut} className='header-btn' id='logout-btn'>Log out</button>
                 {width < 1050 && user &&
                     <button className='send-btn' value='' onClick={() => setIsNavMenu(!isNavMenu)}>
                         <i className="fa-solid fa-bars  "></i>
@@ -75,13 +88,15 @@ const Header = (): JSX.Element => {
                 {user ?
 
                     <div className='header-avatar'>
-                        <div style={{ textDecoration: 'none' }}>
+                        <div style={{textDecoration: 'none'}}>
                             {userData.imgURL ?
-                                <img src={userData.imgURL} alt='avatar' className='user-avatar-header' /> :
-                                <InitialsAvatar name={`${userData.firstName} ${userData.lastName}`} className={'avatar-default-header'} />
+                                <img src={userData.imgURL} alt='avatar' className='user-avatar-header'/> :
+                                <InitialsAvatar name={`${userData.firstName} ${userData.lastName}`}
+                                                className={'avatar-default-header'}/>
                             }
                         </div>
-                        <button className='set-status-btn' onClick={handleStatusClick}><UserStatusIndicator user={appState.userData!} /></button>
+                        <button className='set-status-btn' onClick={handleStatusClick}><UserStatusIndicator
+                            user={appState.userData!}/></button>
                         {isStatusOpen &&
                             <div className='dropdown-menu-status'>
                                 <OutsideClickHandler
